a4e3d21233ff11b4bcc393ecd3fca582eab84eb2
Peter Ullrich <dotup.software@gmail.com>
2019-05-16 09:55:54 +0200

    id

--- src/Mqtt/Message.ts
+++ src/Mqtt/Message.ts
@@ -2,11 +2,11 @@ import { v1 } from 'uuid';
 import { IMessage } from './IMessage';
 
 export abstract class Message<T> implements IMessage {
-  messageId: string;
+  id: string;
   timestamp: string;
 
   constructor() {
-    this.messageId = v1();
+    this.id = v1();
     this.timestamp = new Date().toISOString();
     // tslint:disable-next-line: no-object-literal-type-assertion
   }
