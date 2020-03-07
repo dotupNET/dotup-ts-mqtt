79dff94169562f6692f723fbbca9c8388d3ae780
Peter Ullrich <dotup.software@gmail.com>
2020-03-07 13:32:55 +0100

    ConfigureLogging

--- src/MqttLogging.ts
+++ src/MqttLogging.ts
@@ -1,9 +1,16 @@
-import { getLogger as getLog4jLogger, Logger, configure, Configuration } from "log4js";
+import { getLogger as getLog4jLogger, Log4js, Logger, configure, Configuration } from "log4js";
 
 export type Log4jConfiguration = Configuration
 
-export function ConfigureLogging(config: Log4jConfiguration) {
-  configure(config);
+
+export function ConfigureLogging(filename: string): void;
+export function ConfigureLogging(config: Log4jConfiguration): Log4js;
+export function ConfigureLogging(config: Log4jConfiguration | string): Log4js {
+  if(typeof config ==="string"){
+    return configure(config);
+  }else{
+    return configure(config);
+  }
 }
 
 export function getNamedLogger(loggerName?: string): Logger {
